26f7b9c58048d1284df57159c4c5d8ef376892f2
AnoopEKCS <115262566+AnoopEKCS@users.noreply.github.com>
2025-02-24 20:05:42 +0000

    oneTrust_testAdded

---
+++ assets/custom-script.js
@@ -0,0 +1,38 @@
+//GET WINDOW WIDTH
+$(document).ready(function(){
+  var sw = window.innerWidth;
+  var sh = window.innerHeight;
+  
+  //FOR MOBILE DEVICES ONLY CHANGE SCREEN SIZE
+  // if( sw <= 500 && sh <= 915) { 
+  //    /** FIRST COLLECTION ITEM **/
+  //   $('li.collection--item:nth-child(1) > a:nth-child(1) > figure:nth-child(1) > img:nth-child(2)')
+  //     .attr({
+  //       'src': 'https://cdn.shopify.com/s/files/1/0517/1934/0205/files/ChickenFingersColorblockHoodie.jpg',
+  //       'data-rimg-template' : 'https://cdn.shopify.com/s/files/1/0517/1934/0205/files/ChickenFingersColorblockHoodie.jpg',
+  //       'srcset' : 'https://cdn.shopify.com/s/files/1/0517/1934/0205/files/ChickenFingersColorblockHoodie.jpg'
+  //     });
+    
+  //   /** SECOND COLLECTION ITEM **/
+  //   $('li.collection--item:nth-child(2) > a:nth-child(1) > figure:nth-child(1) > img:nth-child(2)').attr({
+  //       'src': 'https://cdn.shopify.com/s/files/1/0517/1934/0205/files/ChickenFingersColorblockHoodie.jpg',
+  //       'data-rimg-template' : 'https://cdn.shopify.com/s/files/1/0517/1934/0205/files/ChickenFingersColorblockHoodie.jpg',
+  //       'srcset' : 'https://cdn.shopify.com/s/files/1/0517/1934/0205/files/ChickenFingersColorblockHoodie.jpg'
+  //     });
+    
+  //   /** THIRD COLLECTION ITEM **/
+  //   $('li.collection--item:nth-child(3) > a:nth-child(1) > figure:nth-child(1) > img:nth-child(2)').attr({
+  //       'src': 'https://cdn.shopify.com/s/files/1/0517/1934/0205/files/ChickenFingersColorblockHoodie.jpg',
+  //       'data-rimg-template' : 'https://cdn.shopify.com/s/files/1/0517/1934/0205/files/ChickenFingersColorblockHoodie.jpg',
+  //       'srcset' : 'https://cdn.shopify.com/s/files/1/0517/1934/0205/files/ChickenFingersColorblockHoodie.jpg'
+  //     });
+    
+  //   /** FOURTH COLLECTION ITEM **/
+  //   $('li.collection--item:nth-child(4) > a:nth-child(1) > figure:nth-child(1) > img:nth-child(2)').attr({
+  //       'src': 'https://cdn.shopify.com/s/files/1/0517/1934/0205/files/ChickenFingersColorblockHoodie.jpg',
+  //       'data-rimg-template' : 'https://cdn.shopify.com/s/files/1/0517/1934/0205/files/ChickenFingersColorblockHoodie.jpg',
+  //       'srcset' : 'https://cdn.shopify.com/s/files/1/0517/1934/0205/files/ChickenFingersColorblockHoodie.jpg'
+  //     }); 
+  // }
+});
+
